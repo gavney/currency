27ae6a6931c18f2e9aaebcdfdd9a384c39783007
Андрей <lebedev.andrey1303@yandex.ru>
2022-05-31 13:36:11 +0300

    design

--- app/src/main/java/com/example/currency/MainActivity.kt
+++ app/src/main/java/com/example/currency/MainActivity.kt
@@ -15,50 +15,50 @@ import retrofit2.Call
 import kotlin.coroutines.CoroutineContext
 
 
-//class MainActivity : AppCompatActivity(), CoroutineScope {
-//    private lateinit var job: Job
-//    private val service = CurrencyApp.create()
-//
-//    override val coroutineContext: CoroutineContext
-//        get() = job + Dispatchers.Main
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        supportActionBar?.hide()
-//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//        spinnerCreate()
-//
-//        button.setOnClickListener {
-//            service.getRates(spinnerBase.selectedItem.toString(), "sandbox_c9farriad3iampagd6cg")
-//                .enqueue(
-//                    object : retrofit2.Callback<Response> {
-//                        override fun onResponse(
-//                            call: Call<Response>,
-//                            response: retrofit2.Response<Response>
-//                        ) {
-//                            textView.text =
-//                                response.body()?.quote?.get(spinner.selectedItem.toString())
-//                        }
-//
-//                        override fun onFailure(call: Call<Response>, t: Throwable) {
-//                        }
-//                    }
-//                )
-//        }
-//    }
-//
-//    private fun spinnerCreate() {
-//        val adapter = ArrayAdapter(
-//            this,
-//            android.R.layout.simple_spinner_item,
-//            resources.getStringArray(R.array.currencyName)
-//        )
-//        spinner.adapter = adapter
-//        spinnerBase.adapter = adapter
-//
-//        spinner.setSelection(resources.getStringArray(R.array.currencyName).indexOf("RUB"))
-//        spinnerBase.setSelection(resources.getStringArray(R.array.currencyName).indexOf("USD"))
-//    }
-//}
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private lateinit var job: Job
+    private val service = CurrencyApp.create()
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        spinnerCreate()
+
+        button.setOnClickListener {
+            service.getRates(spinnerBase.selectedItem.toString(), "sandbox_c9farriad3iampagd6cg")
+                .enqueue(
+                    object : retrofit2.Callback<Response> {
+                        override fun onResponse(
+                            call: Call<Response>,
+                            response: retrofit2.Response<Response>
+                        ) {
+                            textView.text =
+                                response.body()?.quote?.get(spinner.selectedItem.toString())
+                        }
+
+                        override fun onFailure(call: Call<Response>, t: Throwable) {
+                        }
+                    }
+                )
+        }
+    }
+
+    private fun spinnerCreate() {
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            resources.getStringArray(R.array.currencyName)
+        )
+        spinner.adapter = adapter
+        spinnerBase.adapter = adapter
+
+        spinner.setSelection(resources.getStringArray(R.array.currencyName).indexOf("RUB"))
+        spinnerBase.setSelection(resources.getStringArray(R.array.currencyName).indexOf("USD"))
+    }
+}
