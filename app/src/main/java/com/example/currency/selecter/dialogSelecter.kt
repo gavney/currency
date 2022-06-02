6a6164b85dd3c79ceb2e0f6ecc92dabf8f96483a
Андрей <lebedev.andrey1303@yandex.ru>
2022-06-02 16:41:25 +0300

    design

--- app/src/main/java/com/example/currency/selecter/dialogSelecter.kt
+++ app/src/main/java/com/example/currency/selecter/dialogSelecter.kt
@@ -3,23 +3,27 @@ package com.example.currency.selecter
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import com.example.currency.MainActivity
 import com.example.currency.R
 
-class MyDialogFragment(public val base: Boolean = false) : DialogFragment() {
+class MyDialogFragment(private val base: Boolean = false) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
+
             val builder = AlertDialog.Builder(it)
             val bundle = Bundle()
-            builder.setSingleChoiceItems(R.array.currencyName, -1
-                ) { dialog, item ->
-                    Toast.makeText(activity, "Selected:  ${resources.getStringArray(R.array.currencyName)[item]} $base",
-                        Toast.LENGTH_SHORT).show()
-                }
-                .setPositiveButton("select"
-                ) { dialog, item ->
-                }
+            builder.setSingleChoiceItems(
+                R.array.currencyName, -1
+            ) { _, item ->
+                (activity as MainActivity?)?.getDataFromFragment(
+                    base,
+                    resources.getStringArray(R.array.currencyName)[item]
+                )
+            }
+                .setPositiveButton(
+                    "select"
+                ) { _, item -> }
 
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
